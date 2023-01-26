a3d55edb5c5254a567f11ed0c810d82e48483db8
LucasMoraes98 <lucasotavio98@hotmail.com>
2023-01-25 21:01:44 -0300

    ajuste2

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,13 @@ const backgroundAreas = document.querySelectorAll('.lightdark')
 let ativadoDark = false
 
 const listaPokemon = document.getElementById('listaPokemon')
+const bttMore = document.getElementById('bttMore')
+const divPaginacaoMore = document.getElementById('paginacaoMore')
+const bttAll = document.getElementById('bttAll')
 
 const maxRecords = 649;
-const limit = 649;
 let offset = 0;
+let limit = 10;
 
 function alterarModoEscuroClaro() {
 
@@ -22,9 +25,38 @@ function alterarModoEscuroClaro() {
     }
 }
 
-function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
+function hideLoadMore() {
+    divPaginacaoMore.removeChild(bttMore)
+}
+
+function showLoadMore() {
+    divPaginacaoMore.appendChild(bttMore)
+}
+
+function loadMorePokemons(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((allPokemons = []) => {
+        const newHtml = allPokemons.map(convertPokemonToLi).join('')
+        listaPokemon.innerHTML += newHtml
     })
 }
+
+loadMorePokemons(offset, limit)
+
+bttMore.addEventListener('click', ()=> {
+    offset += limit
+    const qtdRecordNextPage = offset + limit
+
+    if (qtdRecordNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset;
+        loadMorePokemons(offset, newLimit);
+        hideLoadMore();
+    } else {
+        loadMorePokemons(offset, limit)
+    }
+})
+
+bttAll.addEventListener('click', ()=> {
+    listaPokemon.innerHTML = ''
+    loadMorePokemons(0, 30)
+    showLoadMore()
+})
